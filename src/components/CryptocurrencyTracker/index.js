4ebdff4d300ed05dedbd1ef72305b6417359a02f
Prashant Tiwari <149590314+PrashnatTiwari@users.noreply.github.com>
2024-02-27 17:56:32 +0530

    Update index.js

--- src/components/CryptocurrencyTracker/index.js
+++ src/components/CryptocurrencyTracker/index.js
@@ -1,6 +1,6 @@
 import './index.css'
 
-import CryptoCurrenciesList from '../CryptocurrencyItem'
+import CryptoCurrenciesList from '../CryptocurrenciesList'
 
 const CryptocurrencyTracker = () => {
   return (
